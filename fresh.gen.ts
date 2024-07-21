5d0d41fd5f4f16af5fe9c243bf2d95a89245e572
Kai Wendlandt <kaiwendlandt@pm.me>
2024-07-21 21:12:27 +0200

    feat: Change chat to root page

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -5,7 +5,6 @@
 import * as $_404 from "./routes/_404.tsx";
 import * as $_app from "./routes/_app.tsx";
 import * as $api_joke from "./routes/api/joke.ts";
-import * as $chat from "./routes/chat.tsx";
 import * as $greet_name_ from "./routes/greet/[name].tsx";
 import * as $index from "./routes/index.tsx";
 import * as $ChatForm from "./islands/ChatForm.tsx";
@@ -16,7 +15,6 @@ const manifest = {
     "./routes/_404.tsx": $_404,
     "./routes/_app.tsx": $_app,
     "./routes/api/joke.ts": $api_joke,
-    "./routes/chat.tsx": $chat,
     "./routes/greet/[name].tsx": $greet_name_,
     "./routes/index.tsx": $index,
   },
--- routes/index.tsx
+++ routes/index.tsx
@@ -1,26 +1,99 @@
+import { Handlers, PageProps } from "$fresh/server.ts";
+import { Partial } from "$fresh/runtime.ts";
+import { app } from "../graph/base.ts";
+import { ChatForm } from "../islands/ChatForm.tsx";
 import { useSignal } from "@preact/signals";
-import Counter from "../islands/Counter.tsx";
+import { ASK_HUMAN_NODE } from "../graph/nodes/askHuman.ts";
 
-export default function Home() {
-  const count = useSignal(3);
+interface Data {
+  threadId: string;
+  messages: string[];
+  currentInput?: string;
+  isLoading?: boolean;
+}
+
+const LOADING = "Loading";
+
+const sessionId = crypto.randomUUID();
+
+export const handler: Handlers<Data> = {
+  async GET(_req, ctx) {
+    console.log("GET");
+    const messages: string[] = ["Hi", "Hello, How are you?"];
+    const threadId = crypto.randomUUID();
+    console.log({ threadId });
+    return await ctx.render({ messages, threadId });
+  },
+  async POST(req, ctx) {
+    const formData = await req.formData();
+    // console.log({ formData });
+    const newMessage = formData.get("chat-message");
+    // console.log({ newMessage });
+    // console.log(formData.get("all-chat-messages"));
+    const oldMessages = JSON.parse(
+      formData.get("all-chat-messages") as string,
+    ) as string[];
+    const threadId = formData.get("thread-id") as string;
+    // console.log({ oldMessages });
+    //
+    // app.getGraph().drawMermaidPng()
+    const graphConfig = {
+      configurable: {
+        thread_id: threadId,
+      },
+      metadata: {
+        session_id: sessionId,
+      },
+    };
+
+    const graphState = await app.getState(graphConfig);
+    const chatHistory = oldMessages.filter((message) => message !== LOADING);
+    let result;
+    if (!graphState.createdAt) {
+      result = await app.invoke(
+        { input: newMessage, chatHistory },
+        graphConfig,
+      );
+    } else {
+      app.updateState(graphConfig, {
+        values: { input: newMessage },
+        asNode: ASK_HUMAN_NODE,
+      });
+      result = await app.invoke({ input: null }, graphConfig);
+    }
+
+    if (!newMessage || typeof newMessage !== "string")
+      return await ctx.render({ threadId, messages: [] });
+
+    const messages = [
+      ...oldMessages.filter((message) => message !== LOADING),
+      result.lastResponse,
+    ];
+    // console.log({ messages });
+    return await ctx.render({
+      threadId,
+      messages,
+      isLoading: false,
+      currentInput: "",
+    });
+  },
+};
+
+export default function ChatPage(props: PageProps<Data>) {
+  const isLoading = useSignal(props.data.isLoading ?? false);
+  const currentInput = useSignal(props.data.currentInput ?? "");
+  const messages = useSignal(props.data.messages ?? []);
 
   return (
-    <div class="px-4 py-8 mx-auto bg-[#86efac]">
-      <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
-        <img
-          class="my-6"
-          src="/logo.svg"
-          width="128"
-          height="128"
-          alt="the Fresh logo: a sliced lemon dripping with juice"
+    <div className="w-[80vw] min-h-[80vh] mx-auto border-gray-800 rounded border-2 my-[10vh] px-4 py-4">
+      <Partial name="all-chat-messages">
+        <ChatForm
+          threadId={props.data.threadId}
+          messages={messages}
+          isLoading={isLoading}
+          currentInput={currentInput}
         />
-        <h1 class="text-4xl font-bold">Welcome to Fresh</h1>
-        <p class="my-4">
-          Try updating this message in the
-          <code class="mx-2">./routes/index.tsx</code> file, and refresh.
-        </p>
-        <Counter count={count} />
-      </div>
+      </Partial>
     </div>
   );
 }
