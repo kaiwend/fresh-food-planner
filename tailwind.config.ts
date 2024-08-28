a2586e96e8e2bf8091cb986f4560c0d786b32c1c
Kai Wendlandt <kaiwendlandt@pm.me>
2024-08-28 12:39:53 +0200

    feat: Add daisyui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,13 @@
 import { type Config } from "tailwindcss";
+import typography from "typography";
+import daisyui from "daisyui";
 
 export default {
-  content: [
-    "{routes,islands,components}/**/*.{ts,tsx}",
-  ],
+  content: ["{routes,islands,components}/**/*.{ts,tsx}"],
+  // deno-lint-ignore no-explicit-any
+  plugins: [typography, daisyui as any],
+  daisyui: {
+    themes: ["light", "dark", "lemonade", "autumn", "aqua", "nord"],
+    logs: false,
+  },
 } satisfies Config;
